fb74e63a483a85f673af094331158f5cd79fd915
Kirk Woll <kirk.woll@gmail.com>
2016-01-12 14:16:27 -0800

    Add image visualizer

---
+++ Restless/Controls/ResponseVisualizers/ImageResponseVisualizer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using Restless.ViewModels;
+using Restless.WpfExtensions;
+using SexyReact.Views;
+
+namespace Restless.Controls.ResponseVisualizers
+{
+    [ResponseVisualizer("image/jpg", "image/jpeg", "image/png", "image/gif")]
+    public class ImageResponseVisualizer : RxDockPanel<ApiResponseModel>, IResponseVisualizer
+    {
+        public string Header => "Image";
+
+        public ImageResponseVisualizer()
+        {
+            var image = new Image();
+
+            this.Add(image);
+
+            this.Bind(x => x.Response).To(x =>
+            {
+                if (x != null)
+                {
+                    image.Source = BitmapFrame.Create(new MemoryStream(x));
+                }
+            });
+        }
+    }
+}
