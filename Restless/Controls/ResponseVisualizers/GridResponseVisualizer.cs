1e42d9b63f99d440a506638663129e174991a93f
Kirk Woll <kirk.woll@gmail.com>
2016-01-24 05:56:21 -0800

    Allow sorting of columns in grid visualizer

--- Restless/Controls/ResponseVisualizers/GridResponseVisualizer.cs
+++ Restless/Controls/ResponseVisualizers/GridResponseVisualizer.cs
@@ -1,5 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Dynamic;
+using System.Linq;
 using System.Windows.Controls;
+using System.Windows.Data;
 using Newtonsoft.Json.Linq;
 using Restless.ViewModels;
 using Restless.WpfExtensions;
@@ -16,16 +21,35 @@ namespace Restless.Controls.ResponseVisualizers
         {
             var grid = new DataGrid
             {
+//                AutoGenerateColumns = false,
                 CanUserAddRows = false,
                 CanUserDeleteRows = false,
+                CanUserSortColumns = true,
                 IsReadOnly = true,
-                SelectionUnit = DataGridSelectionUnit.Cell
+                SelectionUnit = DataGridSelectionUnit.Cell,
+                HeadersVisibility = DataGridHeadersVisibility.Column
             };
             this.Add(grid);
 
             this.Bind(x => x.JsonResponse).To(_ =>
             {
                 var rows = (JArray)Model.Api.Outputs.Single(x => x.Name == "rows").Value;
+                string currentSortedColumn = null;
+                grid.AutoGeneratingColumn += (sender, args) =>
+                {
+                    args.Column.CanUserSort = true;
+                    args.Column.SortMemberPath = args.PropertyName;
+                };
+                grid.Sorting += (sender, args) =>
+                {
+                    Func<JToken, JToken> keySelector = x => x[args.Column.SortMemberPath];
+                    var newRows = currentSortedColumn == null || currentSortedColumn != args.Column.SortMemberPath ?
+                        rows.OrderBy(keySelector) :
+                        rows.OrderByDescending(keySelector);
+                    currentSortedColumn = currentSortedColumn == args.Column.SortMemberPath ? null : args.Column.SortMemberPath;
+                    grid.ItemsSource = new JArray(newRows);
+                    args.Handled = true;
+                };
                 grid.ItemsSource = rows;
             });
         }
